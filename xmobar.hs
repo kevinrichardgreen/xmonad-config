3a1f463dc4e19db9e83cebb7132ae4c2b1636a8c
Kevin Green <kevin.richard.green@gmail.com>
2019-09-30 21:18:44 -0600

    Increase interval for nordvpn-server update

--- xmobar.hs
+++ xmobar.hs
@@ -20,7 +20,7 @@ Config {
         Run Memory ["-t","M:<usedratio>","-p","2","-L","50","-H","80","-h","red","-l","green","--normal","yellow"] 10,
 	Run Battery ["-t","<acstatus>:<left>","--","-O","AC","-o","Bat","-L","15","-H","50","-h","green","-l","red"] 10,
         Run Date "%m-%d %H:%M" "date" 10,
-        Run Com  "nordvpn-server" ["&"] "" 100
+        Run Com  "nordvpn-server" ["&"] "" 300
     ],
     sepChar = "%",
     alignSep = "}{",
